c22541945eeede57fd7ea631e41f59989a24bced
dgarascenco <54360508+dgarascenco@users.noreply.github.com>
2020-11-05 09:29:40 +0200

    Add files via upload

--- src/user/User.js
+++ src/user/User.js
@@ -8,7 +8,18 @@ class User {
         this.online = null
     }
 
-    render(){
-        
+    render(rootElement, mode = "sm" ){
+        let div = document.createElement('div')
+            div.className = "user-" + mode
+            div.appendChild( document.createElement('img') )
+            div.children[0].src = this.avatar
+
+            if (mode == "md"){
+                let h2 = document.createElement('h2')
+                    h2.innerText = this.nickname
+                div.appendChild(h2)
+            }
+
+        rootElement.appendChild(div)
     }
 }
