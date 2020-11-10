ab0fa0cd3a147f64c303ea0409fa3dd7cdca42e4
dgarascenco <54360508+dgarascenco@users.noreply.github.com>
2020-11-10 15:43:13 +0200

    Add files via upload

--- src/search/Search.js
+++ src/search/Search.js
@@ -6,34 +6,90 @@ class Search {
     }
 
     render (root){
+        ///////      главный блок поиска
         let div = document.createElement('div')
             div.classList.add("search")
-        let input = document.createElement('input')
-        let divResult = document.createElement('div')
-             divResult.classList.add("result")
 
-        switch (this.type){
-            case 'nickname':   input.placeholder = "Search for users..."
-            ////// more options...
-        }
+        ////////////////поиск по имени пользователя
+        let input = document.createElement('input')
+            input.placeholder = "Search for" + this.type
 
         div.appendChild(input)
-        div.appendChild(divResult)
         root.appendChild(div)
 
-        let searchArray = this.searchArray
-        input.addEventListener("keyup", function (e){
-                                            let div = document.querySelector('.result')
-                                                div.textContent = ""
+        ///////////// блок фильтра по полу
+        let div_gender = document.createElement('div')
+            div_gender.className = "gender"
+            div.appendChild(div_gender)
+        this.addGender('all', true)
+        this.addGender('male')
+        this.addGender('female')
+        this.addGender('undecided')
 
-                                                for (i=0; i<searchArray.length; i++)
-                                                    if ( searchArray[i].toLowerCase().indexOf(e.target.value.toLowerCase()) != -1){
-                                                        console.log(searchArray[i])
-                                                        let h = document.createElement('h4')
-                                                        h.innerText  = searchArray[i]
-                                                        div.appendChild(h)
-                                                    }    
-                                        })
+        /////////// блок онлайн
+        let div_online = document.createElement('div')
+            div_online.className = "online"
+        let input_online = document.createElement('input')
+            input_online.setAttribute("type", "checkbox")
+            input_online.id = "online"
+            input_online.value = "true"
+        let label = document.createElement("label")
+            label.htmlFor = "online"
+            label.innerText = "On Line Only (default: off line)"
+
+            div_online.appendChild(input_online)
+            div_online.appendChild(label)
+            div.appendChild(div_online)
+
+        ////////////блок вывода результата    
+        let divResult = document.createElement('div')
+            divResult.classList.add("result")
+            div.appendChild(divResult)
+
+            let searchArray = this.searchArray
+ 
+         input.addEventListener("keyup", eventSearch)
+        div.addEventListener("click", eventSearch)
+        
+        function eventSearch (e) {
+
+            let div = document.querySelector('.result')
+                div.textContent = ""
+            let gender = document.querySelector( 'input[name="gender"]:checked');                               
+            let online = document.querySelector('.online').firstChild
+            let text = document.querySelector('.search').firstChild
+
+           console.log( e.target )
+
+            if ( online.checked == true )  {
+                searchArray.filter( value => value.nickname.toLowerCase().indexOf(text.value.toLowerCase()) != -1 )
+                            .filter( value => gender.value.toLowerCase().indexOf(value.gender.toLowerCase()) != -1 )                           
+                            .filter( value => value.online == online.checked )
+                            .filter( value => value.render( document.querySelector('.result'), "search" ) )
+            }////////// не знал как обойти ситуацию true/false 
+            else {
+                searchArray.filter( value => value.nickname.toLowerCase().indexOf(text.value.toLowerCase()) != -1 )
+                            .filter( value => gender.value.toLowerCase().indexOf(value.gender.toLowerCase()) != -1 )                                                           
+                            .filter( value => value.render( document.querySelector('.result'), "search" ) )                                                                                                                     
+            }
+        }                                        
+                    
        
+    }
+    /////////// Добавление очередного пола
+    addGender(title, check = false){       
+        let div = document.querySelector(".gender")
+        let radio = document.createElement("input")
+            radio.setAttribute("type", "radio")
+            radio.id = "gender_" + title
+            if (title == "all") radio.value = "allmalefemaleundecided"   /// некорректно получилось
+            else radio.value = title
+            radio.name = "gender"
+            radio.checked = check
+        let label = document.createElement("label")
+            label.htmlFor = "gender_" + title
+            label.innerText = title
+            div.appendChild(radio)
+            div.appendChild(label)
     }
 }
